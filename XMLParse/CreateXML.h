161671bd2fe3422550695053c3ceb2f81da60e2a
Jung <bakkim05@gmail.com>
2018-05-21 23:05:14 -0600

    pugiXML test -> Bertha

---
+++ XMLParse/CreateXML.h
@@ -0,0 +1,12 @@
+//
+// Created by Juno on 5/21/2018.
+//
+
+#ifndef XMLPARSE_CREATEXML_H
+#define XMLPARSE_CREATEXML_H
+
+#include "pugixml.hpp"
+
+void create(char* song1, char* gender1, char* artist1, char* album1, char* year1, char* lyrics1, char* category1);
+
+#endif //XMLPARSE_CREATEXML_H
