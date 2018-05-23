54a6a987fc4f3d133b4e08dc7a1a8632eb6194e0
Bakkim05 <bakkim05@gmail.com>
2018-05-23 04:04:58 -0600

    Clean Up and Update MakeFile

--- c++/mserver/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ c++/mserver/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -50,5 +50,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/mserver.dir/DependInfo.cmake"
+  "CMakeFiles/Test.dir/DependInfo.cmake"
   )
---
+++ c++/mserver/cmake-build-debug/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/main.cpp.obj"
+  "CMakeFiles/Test.dir/writedats.cpp.obj"
+  "CMakeFiles/Test.dir/Maker.cpp.obj"
+  "CMakeFiles/Test.dir/cJSON.c.obj"
+  "CMakeFiles/Test.dir/server.cpp.obj"
+  "CMakeFiles/Test.dir/tcpserver.cpp.obj"
+  "Test.pdb"
+  "Test.exe"
+  "Test.exe.manifest"
+  "libTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
