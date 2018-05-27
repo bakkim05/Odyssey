fa187027cb992aa35247cd0f9941ae44cb53e13d
Unknown <faridmarin25@gmail.com>
2018-05-26 19:09:08 -0600

    index on develop: b5c0771 Auto stash before merge of "develop" and "origin/develop"

--- java/Odyssey/src/application/MainPController.java
+++ java/Odyssey/src/application/MainPController.java
@@ -10,12 +10,20 @@ import javax.xml.stream.XMLStreamException;
 import javaclient.SocketClient;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.media.MediaPlayer.Status;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import mediaPlayer.Player;
 import other.ErrorHandlersK;
@@ -24,6 +32,7 @@ import xml.writer;
 public class MainPController {
 	
 	Player player = new Player();
+	public ChoiceBox<String> cb_Options;
 	public Button btn_playPause;
 	public TextField txt_songName;
 	public TextField txt_songArtist;
@@ -32,9 +41,21 @@ public class MainPController {
 	public BarChart<?,?> sbc_groovBox;
 	ErrorHandlersK errHand = new ErrorHandlersK();
 	List MetaSongsList = new ArrayList<>();
-
+	@FXML
+	private void initialize() {
+		
+		cb_Options.setItems(FXCollections.observableArrayList(
+			    "The Gene game!", "Log out", "Delete my account"));
+		
+		startAllListeners();
+		 
+	}
+	 
 	
 
+	
+	
+	//player.Encode()
 	public void playPauseMedia() {
 		
 		// Playing and pausing music
@@ -65,11 +86,7 @@ public class MainPController {
 		
 		
 	}
-	
-	
-	//player.Encode()
 
-	
 	public void loadASong() {
 		
 		try {
@@ -103,8 +120,6 @@ public class MainPController {
 		return sock.message;
 	}
 	
-	
-	
 	public void searchSong() throws Exception{
 		
 	if("good" == errHand.createMusicError(txt_songName.getText().toString(), txt_songArtist.getText().toString())) {
@@ -128,10 +143,6 @@ public class MainPController {
 		
 	}
 	
-	
-	
-	
-	
 	private void startAllListeners() {
 		
 		// slider listener
@@ -154,8 +165,60 @@ public class MainPController {
 		//
 		
 		
+		//cb_Options Listener
+		
+		cb_Options.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+				if(newValue.equals(0)) {// The gene Game!
+					try {
+						goToGenePage();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}else if(newValue.equals(1)) {// Log out
+					try {
+						goToStartPage();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}else if(newValue.equals(2)) {// Delete my account
+					
+				}
+				
+			}
+			
+		});
+		
+		// 
+		
 	}
 	
+	private void goToGenePage() throws IOException {
+		
+		Parent gui = FXMLLoader.load(getClass().getResource("/odyGUI/GenPage.fxml"));
+		Scene genePage = new Scene(gui);
+		
+		//this line optains the stage information
+		Stage window = (Stage)txt_songName.getScene().getWindow();
+		window.setScene(genePage); // txt_createUser is needed here to obtain the window its in
+		window.show();
+		
+	}
+	
+	private void goToStartPage() throws IOException {
+		
+		Parent gui = FXMLLoader.load(getClass().getResource("/odyGUI/StartPage.fxml"));
+		Scene startPage = new Scene(gui);
+		
+		//this line optains the stage information
+		Stage window = (Stage)txt_songName.getScene().getWindow();
+		window.setScene(startPage); // txt_createUser is needed here to obtain the window its in
+		window.show();
+		
+	}
 	
 	
 }
